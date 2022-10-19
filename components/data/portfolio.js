28baa7aa2d306f49ce0e7f47be91de78889671b3
Oyucho <bellahoyucho30@gmail.com>
2022-10-19 09:59:07 +0300

    first commit

---
+++ components/data/portfolio.js
@@ -0,0 +1,22 @@
+const portfolio = [
+  {
+    id: "01",
+    title: "Transportation Mobile App",
+    img: "/images/portfolio-01.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+  {
+    id: "02",
+    title: "Workout Tracker App",
+    img: "/images/portfolio-02.jpg",
+    category: "Mobile App",
+    keyword: ["Mobile", "App", "UI-UX"],
+    liveUrl: "#",
+  },
+
+];
+
+export default portfolio;
